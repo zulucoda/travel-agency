0abf22934b540a967a8571fd8867619abf845c8f
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2018-06-16 20:07:59 +0200

    [muzi] adding enzyme for creating view (react components) :fist:

---
+++ src/setupTests.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Muzikayise Flynn Buthelezi (zuluCoda) on 2018/06/16.
+ * Copyright mfbproject.co.za - muzi@mfbproject.co.za
+ * Copyright zulucoda - mfbproject
+ */
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
