272b8e7b055ccede20eea2ea94893ce1557489ac
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2018-06-19 10:32:00 +0200

    [muzi] adding travel deals class :fist:

---
+++ src/service/test/travel-delas.spec.js
@@ -0,0 +1,116 @@
+import TravelDeals from '../travel-deals';
+import testData from '../../../public/data/response.json'; //using as test data as well for convienecey
+
+describe('Travel Deals - Unit Test', () => {
+  describe('getTravelDealsForCities', () => {
+    describe('cheapest deals', () => {
+      it('should get travel deals for current departure and arrival city', () => {
+        const departure = 'London';
+        const arrival = 'Moscow';
+        const dealType = 'Cheapest';
+
+        const actor = new TravelDeals(testData, dealType);
+
+        const actual = actor.getTravelDealsForCities(departure, arrival);
+
+        const expected = {
+          deals: [
+            {
+              transport: 'bus',
+              departure: 'London',
+              arrival: 'Amsterdam',
+              duration: { h: '07', m: '45' },
+              cost: 40,
+              discount: 25,
+              reference: 'BLA0745'
+            },
+            {
+              transport: 'bus',
+              departure: 'Amsterdam',
+              arrival: 'Warsaw',
+              duration: { h: '05', m: '15' },
+              cost: 40,
+              discount: 25,
+              reference: 'BAW0515'
+            },
+            {
+              transport: 'bus',
+              departure: 'Warsaw',
+              arrival: 'Stockholm',
+              duration: { h: '05', m: '15' },
+              cost: 40,
+              discount: 50,
+              reference: 'BWS0515'
+            },
+            {
+              transport: 'bus',
+              departure: 'Stockholm',
+              arrival: 'Moscow',
+              duration: { h: '05', m: '30' },
+              cost: 40,
+              discount: 25,
+              reference: 'BSM0530'
+            }
+          ]
+        };
+
+        expect(actual).toEqual(expected);
+      });
+    });
+
+    describe('fastest deals', () => {
+      it('should get travel deals for current departure and arrival city', () => {
+        const departure = 'London';
+        const arrival = 'Moscow';
+        const dealType = 'Fastest';
+
+        const actor = new TravelDeals(testData, dealType);
+
+        const actual = actor.getTravelDealsForCities(departure, arrival);
+
+        const expected = {
+          deals: [
+            {
+              transport: 'car',
+              departure: 'London',
+              arrival: 'Amsterdam',
+              duration: { h: '04', m: '45' },
+              cost: 120,
+              discount: 0,
+              reference: 'CLA0445'
+            },
+            {
+              transport: 'car',
+              departure: 'Amsterdam',
+              arrival: 'Warsaw',
+              duration: { h: '04', m: '45' },
+              cost: 120,
+              discount: 0,
+              reference: 'CAW0445'
+            },
+            {
+              transport: 'bus',
+              departure: 'Warsaw',
+              arrival: 'Stockholm',
+              duration: { h: '05', m: '15' },
+              cost: 40,
+              discount: 50,
+              reference: 'BWS0515'
+            },
+            {
+              transport: 'car',
+              departure: 'Stockholm',
+              arrival: 'Moscow',
+              duration: { h: '04', m: '00' },
+              cost: 120,
+              discount: 0,
+              reference: 'CSM0400'
+            }
+          ]
+        };
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+});
---
+++ src/service/travel-deals.js
@@ -0,0 +1,137 @@
+const CITY_TYPE = { departure: 'departure', arrival: 'arrival' };
+const DEAL_TYPE = { cheapest: 'Cheapest', fastest: 'Fastest' };
+
+class TravelDeals {
+  constructor(data, dealType) {
+    this.data = data;
+    this.dealType = dealType;
+  }
+
+  _findCityAndIndex(cityType, city) {
+    let firstIndex = -1;
+    const deals = this.data.deals.filter((item, index) => {
+      if (item[cityType].toLowerCase() === city.toLowerCase()) {
+        if (firstIndex === -1) {
+          firstIndex = index;
+        }
+        return true;
+      }
+    });
+
+    return {
+      deals: deals.slice(0, 3),
+      index: firstIndex,
+      city,
+      cityType
+    };
+  }
+
+  _mapJourney(departureCityAndIndex, arrivalCityAndIndex) {
+    const deals = this.data.deals.slice(
+      departureCityAndIndex.index,
+      arrivalCityAndIndex.index
+    );
+    const mapJourney = [];
+    mapJourney.push(arrivalCityAndIndex);
+    for (let i = deals.length - 1; i > -1; i = i - 2) {
+      if (
+        deals[i].arrival ===
+        mapJourney[mapJourney.length - 1].deals[0].departure
+      ) {
+        const city = this._findCityAndIndex(
+          CITY_TYPE.arrival,
+          deals[i].arrival
+        );
+        mapJourney.push(city);
+      }
+    }
+    return mapJourney.reverse();
+  }
+
+  _calculateCheapestDeal(journey) {
+    return journey.deals.reduce((dealAcc, currentDeal) => {
+      const acc = dealAcc.cost ? dealAcc.cost : dealAcc;
+      return Math.min(acc, currentDeal.cost);
+    });
+  }
+
+  _filterCheapestDeal(journey, cheapestDeal) {
+    return [journey.deals.filter(deal => deal.cost === cheapestDeal)[0]]; // always return first one if there's more than one
+  }
+
+  _getCheapestDeals(mappedJourney) {
+    mappedJourney.forEach(journey => {
+      const cheapestDeal = this._calculateCheapestDeal(journey);
+      journey.deals = this._filterCheapestDeal(journey, cheapestDeal);
+      return journey;
+    });
+    return {
+      deals: mappedJourney.map(deals => {
+        return deals.deals.reduce(deal => deal);
+      })
+    };
+  }
+
+  _calculateFastestDeal(journey) {
+    return journey.deals.reduce((dealAcc, currentDeal) => {
+      const acc = dealAcc.reference ? dealAcc.reference : dealAcc;
+      return Math.min(
+        Number.isInteger(acc) ? acc : parseInt(acc.substring(4)),
+        parseInt(currentDeal.reference.substring(4))
+      );
+    });
+  }
+
+  _getBestDealForCustomer(journey, fastestDeal) {
+    // if there's more than fast deal, get the cheapest for the customer
+    const fastestJourney = journey.deals.filter(
+      deal => parseInt(deal.reference.substring(4)) === fastestDeal
+    );
+    if (fastestJourney.length > 1) {
+      const bestDealForCustomer = this._calculateCheapestDeal(journey);
+      return this._filterCheapestDeal(journey, bestDealForCustomer);
+    }
+    return [fastestJourney[0]];
+  }
+
+  _getFastestDeals(mappedJourney) {
+    mappedJourney.forEach(journey => {
+      const fastestDeal = this._calculateFastestDeal(journey);
+      journey.deals = this._getBestDealForCustomer(journey, fastestDeal); // always return first one if there's more than one
+      return journey;
+    });
+    return {
+      deals: mappedJourney.map(deals => {
+        return deals.deals.reduce(deal => deal);
+      })
+    };
+  }
+
+  _getPreferredJourney(mappedJourney) {
+    if (this.dealType === DEAL_TYPE.cheapest) {
+      return this._getCheapestDeals(mappedJourney);
+    }
+    return this._getFastestDeals(mappedJourney);
+  }
+
+  getTravelDealsForCities(departure, arrival) {
+    const departureCityAndIndex = this._findCityAndIndex(
+      CITY_TYPE.departure,
+      departure
+    );
+    const arrivalCityAndIndex = this._findCityAndIndex(
+      CITY_TYPE.arrival,
+      arrival
+    );
+
+    // Because the initial data is an ordered list we can use this to find cities in between departure & arrival and map the journey
+    const mappedJourney = this._mapJourney(
+      departureCityAndIndex,
+      arrivalCityAndIndex
+    );
+
+    return this._getPreferredJourney(mappedJourney);
+  }
+}
+
+export default TravelDeals;
